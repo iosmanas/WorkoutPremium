d17cd27ce1227e12b4f2d831fce9c1e26ba995b4
Manas Duishoev <manasduishoev034@gmail.com>
2023-02-09 14:56:49 +0600

    Initial Commit

---
+++ WorkoutPremium/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutPremium
+//
+//  Created by new on 9/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
